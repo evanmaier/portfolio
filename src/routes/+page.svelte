e5267a96a2996b9e8edf5e6ff4149404a41e390c
Evan Maier <evanm413r@gmail.com>
2024-08-21 14:43:29 -0700

    finish landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,13 @@
-<script lang="ts">
-    export let title = "Welcome to my portfolio";
-</script>
-
-<main>
-    <h1>{title}</h1>
-    <p>Hi, I'm Evan Maier, an Electrical Engineer and Software Developer. Welcome to my portfolio!</p>
-    <!-- Add hero section, introduction, etc. -->
-</main>
-
-<a href="/about">about</a>
-<a href="/experience">experience</a>
-<a href="/contact">contact</a>
+<div class="hero bg-base-200 min-h-screen">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">Hello there</h1>
+        <p class="py-6">
+          My name is Evan Maier. I'm an Electrical Engineer with a background in software and firmware development.
+        </p>
+        <a href="\about" role="button" class="btn btn-primary">About</a>
+        <a href="\projects" role="button" class="btn btn-primary">Projects</a>
+        <a href="\contact" role="button" class="btn btn-primary">Contact</a>
+      </div>
+    </div>
+  </div>
