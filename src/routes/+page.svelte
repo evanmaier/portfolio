98a109581f73a3bea2fce1c42c857efd1ec18094
Evan Maier <evanm413r@gmail.com>
2024-08-23 14:58:28 -0700

    add splashcreen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,26 @@
 <main>
-  <div class="hero bg-base-200 min-h-screen">
-    <div class="hero-content text-center">
-      <div class="max-w-md">
-        <div class="avatar">
-          <div class="w-64 rounded-full">
-            <img src="src/lib/assets/profile3.jpg" alt="profile" class="avatar">
-          </div>
+  <div
+  class="hero min-h-screen"
+  style="background-image: url('src/lib/assets/splashscreen2.png');">
+  <div class="hero-overlay bg-opacity-60"></div>
+  <div class="hero-content text-neutral-content text-center">
+    <div class="max-w-md">
+      <div class="avatar">
+        <div class="w-64 rounded-full">
+          <img src="src/lib/assets/profile3.jpg" alt="profile" class="avatar">
         </div>
-        <h1 class="text-5xl font-bold">Hello there</h1>
-        <p class="py-6">
-          My name is Evan Maier. I'm an Electrical Engineer with a background in software and firmware development.
-        </p>
-        <a href="\about" role="button" class="btn btn-primary">About</a>
-        <a href="\projects" role="button" class="btn btn-primary">Projects</a>
-        <a href="\contact" role="button" class="btn btn-primary">Contact</a>
       </div>
+      <h1 class="text-5xl font-bold">Hello There
+      </h1>
+      <p class="py-6">
+        My name is Evan Maier. I'm an Electrical Engineer with a background in software and firmware development.
+      </p>
+      <a href="\about" role="button" class="btn btn-primary">About</a>
+      <a href="\projects" role="button" class="btn btn-primary">Projects</a>
+      <a href="\contact" role="button" class="btn btn-primary">Contact</a>
+      <a href="https://github.com/evanmaier" role="button" class="btn btn-primary">Github</a>
+      <a href="https://www.linkedin.com/in/evan-maier-6310b1153/" role="button" class="btn btn-primary">Linkedin</a>
     </div>
   </div>
+</div>
 </main>
