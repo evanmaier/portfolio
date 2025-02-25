2a74306e910ecbdff07dfc82a79cb85975796577
Evan Maier <evanm413r@gmail.com>
2025-02-24 17:31:31 -0800

    replace mp4 with youtube link

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -7,8 +7,6 @@
   import front from '$lib/assets/front.png';
   import inside from '$lib/assets/inside.png';
   import box from '$lib/assets/3d_box.png';
-  import demo from '$lib/assets/TunerDemo.mp4';
-
 </script>
 
   <div class="flex flex-col justify-center items-center">
@@ -19,39 +17,52 @@
         I chose to build a chromatic tuner because it poses interesting hardware and software design problems.
         After months of design, testing, and iteration I am quite pleased with the final result.
       </p>
-      <!-- svelte-ignore a11y-media-has-caption -->
-      <video controls autoplay loop muted width="640">
-        <source src={demo} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/g4U_7SbJp7o"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+
       <p>
         The pedal is powered by either a 9V battery or 9V DC power supply, and drives a 1.8" TFT display.
         When the switch is not pressed the guitar signal will bypass the pedal, and when pressed it will mute the output.
       </p>
+
       <figure>
         <img src={front} alt='front' class="rounded-lg object-cover"/>
         <figcaption>Action shot</figcaption>
       </figure>
+
       <p>
         The PCB contains an amplifier circuit, power delivery, connections for the microcontroller, and peripherals.
         The design was done in KiCad and manufactured by JLCPCB.
       </p>
+
       <figure>
         <img src={inside} alt='inside' class="rounded-lg object-cover"/>
         <figcaption>The guts</figcaption>
       </figure>
+
       <p>
         The firmware is written in C and runs on an STM32F411 microcontroller.
         The pitch detection is my implementation of the time domain YIN algorithm deveolped by Cheveigne and Kawahara.
       </p>
+
       <p>
         I was able to achieve 0.5 cent accuracy in the guitar frequency range, which is on par with commercially available alternatives.
         The firmware is available at <a href="https://github.com/evanmaier/tuner-firmware">github.com/evanmaier/tuner-firmware</a> 
       </p>
+
       <figure>
         <img src={box} alt='box' class="rounded-lg object-cover"/>
         <figcaption>Enclosure with items for scale</figcaption>
       </figure>
+
       <p>
         I enlised the help of my friend Karan Dere to design and print the enclosure from my mock-up drawings.
         Check him out at <a href="https://www.linkedin.com/in/karan-dere-070b29154">linkedin.com/in/karan-dere-070b29154</a>.
