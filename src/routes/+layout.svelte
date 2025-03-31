5a7066df7927aff2d8ef9a007201f24792e0bd20
Evan Maier <evanm413r@gmail.com>
2025-03-30 22:14:38 -0700

    refactor projects

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,9 @@
     <nav class="navbar bg-transparent">
         <div class="flex mx-auto space-x-4">
             <a href="/" role="button" class="font-semibold text-lg md:text-2xl">Home</a>
+            <a href="/embedded" role="button" class="font-semibold text-lg md:text-2xl">Embedded</a>
+            <a href="/software" role="button" class="font-semibold text-lg md:text-2xl">Software</a>
+            <a href="/capstone" role="button" class="font-semibold text-lg md:text-2xl">Capstone</a>
         </div>
     </nav>
 
