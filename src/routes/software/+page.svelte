5cb66b7af5d98c966f5f453b03619fb44302e9fa
Evan Maier <evanm413r@gmail.com>
2025-04-01 12:22:25 -0700

    rework tunelink project page

--- src/routes/software/+page.svelte
+++ src/routes/software/+page.svelte
@@ -4,37 +4,74 @@
     import request from '$lib/assets/tunelink-request.png';
 </script>
 
-<article class="prose p-4">
-    <h2 class="text-center">TuneLink</h2>
-    <p>
-      TuneLink is a sharing economy platform for music gear. The goal is to connect
-      local musicians looking to rent and lend instruments and other music equiptment.
-      It also serves as a platform for local buisinesses to advertise rentals and manage
-      inventory.
-    </p>
+<div class="flex flex-col mx-auto items-center p-4 max-w-6xl gap-4">
+  <h1 class="text-4xl font-bold mb-6">TuneLink Web App</h1>
 
-    <p>
-      The core web app was built by me over a two month span in Sept-Oct of 2024.
-      This is a full stack application built with Sveltekit with TypeScript and Firebase.
-    </p>
+  <figure>
+    <img src={tunelink} alt = 'homepage' class="rounded-lg object-cover"/>
+    <figcaption>Landing page where users can see local gear and keyword search</figcaption>
+  </figure>
 
-    <p>
-      Check out the live site at <a href="https://tunelink-alpha.vercel.app/">tunelink-alpha.vercel.app</a> 
-      and the source code at <a href="https://github.com/evanmaier/tunelink">github.com/evanmaier/tunelink</a> 
-    </p>
+  <figure>
+    <img src={rentals} alt = 'rentals' class="rounded-lg object-cover"/>
+    <figcaption>Rentals page where users can manage active and pending rentals</figcaption>
+  </figure>
 
-    <figure>
-      <img src={tunelink} alt = 'homepage' class="rounded-lg object-cover"/>
-      <figcaption>Landing page where users can see local gear and keyword search</figcaption>
-    </figure>
+  <figure>
+    <img src={request} alt = 'request' class="rounded-lg object-cover"/>
+    <figcaption>Requests page where users can interact with a request and chat with the renter/owner</figcaption>
+  </figure>
 
-    <figure>
-      <img src={rentals} alt = 'rentals' class="rounded-lg object-cover"/>
-      <figcaption>Rentals page where users can manage active and pending rentals</figcaption>
-    </figure>
-
-    <figure>
-      <img src={request} alt = 'request' class="rounded-lg object-cover"/>
-      <figcaption>Requests page where users can interact with a request and chat with the renter/owner</figcaption>
-    </figure>
+  <article class="prose max-w-full mt-4">
+    <h2 class="text-2xl font-bold text-center">Outcomes</h2>
+    <ul class="list-disc text-lg">
+      <li>Demo the live site at <a href="https://tunelink-alpha.vercel.app/">tunelink-alpha.vercel.app</a></li>
+      <li>Developed a fully functional instrument-rental platform using SvelteKit and Firebase.</li>
+      <li>Implemented user authentication with Firebase Authentication.</li>
+      <li>Designed an intuitive search experience using Algolia InstantSearch for instrument listings.</li>
+      <li>Created a rental request system with manual approval by owners and integrated real-time chat using Firestore subcollections.</li>
+    </ul>
+  
+    <h2 class="text-2xl font-bold text-center">Challenges & Solutions</h2>
+    <ul class="list-disc text-lg">
+      <li>
+        Managing real-time messaging efficiently with Firestore subcollections.
+        <ul>
+          <li class=" font-light">Optimized Firestore queries and used Firestore listeners for real-time updates.</li>
+        </ul>
+      </li>
+      <li>
+        Handling dynamic rental requests with different statuses (pending, confirmed, rejected).
+        <ul>
+          <li class=" font-light">Designed a structured Firestore schema and used SvelteKit actions for controlled state updates.</li>
+        </ul>
+      </li>
+      <li>
+        Implementing secure and scalable authentication with Firebase while ensuring proper client-server separation.
+        <ul>
+          <li class=" font-light">Used Firebase Admin SDK on the server side for secure account creation and client-side authentication for login sessions.</li>
+        </ul>
+      </li>
+      <li>
+        Ensuring fast and relevant search results for users looking for instruments.          
+        <ul>
+          <li class=" font-light">Leveraged Algolia indexing for real-time, typo-tolerant search functionality.</li>
+        </ul>
+      </li>
+      <li>
+        Displaying geographically relevant results.
+        <ul>
+          <li class=" font-light">Integrated Mapbox Geocoding API</li>
+        </ul>
+      </li>
+    </ul>
+  
+    <h2 class="text-2xl font-bold text-center">Skills Developed</h2>
+    <ul class="list-disc text-lg">
+      <li>Full-Stack Development using SvelteKit and Firebase.</li>
+      <li>Advanced UI/UX design with Tailwind CSS & DaisyUI.</li>
+      <li>Geolocation with Mapbox API.</li>
+      <li>Real-time database management with Firestore and Firestore listeners.</li>
+    </ul>
   </article>
+</div>
