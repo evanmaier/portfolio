0783e30906fda563067cc663456e2883f22396ee
Evan Maier <evanm413r@gmail.com>
2025-04-01 12:38:04 -0700

    update capstone project page

--- src/routes/capstone/+page.svelte
+++ src/routes/capstone/+page.svelte
@@ -1,30 +1,29 @@
 <script>
-  import svpwm_bc from '$lib/assets/svpwm_bc.png';
   import prototype from '$lib/assets/final_prototype.png';
 </script>
-
-<article class="prose p-4 w-full">
-  <h2 class="text-center">Capstone</h2>
-  <p>
-    For our capstone project, my team decided to build an Electronic Speed controller
-    for a brushless DC motor. My role was to design and program the control system. 
-    This involved simulating the system in MATLAB to tune the PID gain parameters, then programming
-    the system on an STM32F407 microcontroller. 
-  </p>
-
-  <p>
-    We chose the Field Oriented Control algorithm with Space Vector PWM commutation.
-    The source code is hosted at <a href="https://github.com/evanmaier/hermes-firmware">github.com/evanmaier/hermes-firmware</a>
-  </p>
-
+  
+<div class="flex flex-col mx-auto items-center p-4 max-w-6xl gap-4">
+  <h1 class="text-4xl font-bold mb-6">BLDC Motor Controller</h1>
+  
   <figure>
     <img src={prototype} alt = 'prototype' class="rounded-lg object-cover"/>
     <figcaption>Demo prototype</figcaption>
   </figure>
 
-  <figure>
-    <img src={svpwm_bc} alt = 'svpwm_b' class="rounded-lg object-cover"/>
-    <figcaption>SVPWM control signal</figcaption>
-  </figure>
-</article>
+  <article class="prose max-w-full mt-4">
+    <h2 class="text-2xl font-bold text-center">Outcomes</h2>
+    <ul class="list-disc text-lg">
+      <li>Deigned and assembled open source brushless DC motor controller in a three person team</li>
+      <li>Presented to a panel of professional engineers, receiving a grade of 87%</li>
+      <li>Successfully implemented space vector control PWM signals to set the motor speed</li>
+      <li>Simulated the PID parameters in MATLAB to optimize stability and response time</li>
+    </ul>
   
+    <h2 class="text-2xl font-bold text-center">Skills Developed</h2>
+    <ul class="list-disc text-lg">
+      <li>PCB design in Altium</li>
+      <li>Control system design using MATLAB Simulink</li>
+      <li>Embedded programming in C</li>
+    </ul>
+  </article>
+</div>
